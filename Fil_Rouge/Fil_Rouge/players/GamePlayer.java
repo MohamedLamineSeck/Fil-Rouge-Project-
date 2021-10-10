592f33c05d69f997c3eef7f9bc8ff5996308f10c
MohamedLamineSeck <92259122+MohamedLamineSeck@users.noreply.github.com>
2021-10-10 16:09:56 +0200

    Add files via upload

---
+++ Fil_Rouge/Fil_Rouge/players/GamePlayer.java
@@ -0,0 +1,8 @@
+package Players;
+import java.util.*;
+
+public interface GamePlayer {
+
+// Methode ChooseMove que va implementer tous les types de joueurs.
+	public int ChooseMove(AbstractGame game);	
+}
